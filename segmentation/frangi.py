precision recall curve fixed + frangi 3D added

segmentation/frangi.py
@@ -9,53 +9,58 @@ import numpy as np
 import math
 import scipy.signal as sc
 import time
-import cv2
 import numpy.linalg as lin
+from scipy.ndimage import filters
 #%%
-
-# create 2D Hessian kernels to later convolve with the image
-def frangi_hessian(src, scale):
-    half_kernel = math.ceil(3 * scale)
-    n_kern_x = 2 * half_kernel + 1
-    n_kern_y = n_kern_x
+# filters given image based on scale and returns output image with vesselness values
+def vesselness_2D(src, scale, beta, c):
+    # create 2D Hessian kernels to later convolve with the image
+    # half_kernel = math.ceil(3 * scale)
+    # n_kern_x = 2 * half_kernel + 1
+    # n_kern_y = n_kern_x
     
-    # Hessian kernels
-    kern_xx_f = np.zeros((n_kern_x, n_kern_y), dtype=float)  # kernel to create Dxx matrix
-    kern_xy_f = np.zeros((n_kern_x, n_kern_y), dtype=float)  # kernel to create Dxy matrix
-    kern_yy_f = np.zeros((n_kern_x, n_kern_y), dtype=float)  # kernel to create Dyy matrix
+    # # Hessian kernels
+    # kern_xx_f = np.zeros((n_kern_x, n_kern_y), dtype=float)  # kernel to create Dxx matrix
+    # kern_xy_f = np.zeros((n_kern_x, n_kern_y), dtype=float)  # kernel to create Dxy matrix
+    # kern_yy_f = np.zeros((n_kern_x, n_kern_y), dtype=float)  # kernel to create Dyy matrix
     
     s2 = scale * scale
-    half_PI_s6 = 1.0 / (2.0 * math.pi * (s2**3))
+    # half_PI_s6 = 1.0 / (2.0 * math.pi * (s2**3))
     
-    for x in range(-half_kernel, half_kernel + 1):
-        x2 = x**2
-        for y in range(-half_kernel, half_kernel + 1):
-            y2 = y**2
-            kern_xx_f[x + half_kernel][y + half_kernel] = half_PI_s6 * (x2  - s2) * math.exp(-(x2 + y2) / (2.0 * s2))
-            kern_xy_f[x + half_kernel][y + half_kernel] = half_PI_s6 * (x * y) * math.exp(-(x2 + y2) / (2.0 * s2))
+    # for x in range(-half_kernel, half_kernel + 1):
+    #     x2 = x**2
+    #     for y in range(-half_kernel, half_kernel + 1):
+    #         y2 = y**2
+    #         kern_xx_f[x + half_kernel][y + half_kernel] = half_PI_s6 * (x2  - s2) * math.exp(-(x2 + y2) / (2.0 * s2))
+    #         kern_xy_f[x + half_kernel][y + half_kernel] = half_PI_s6 * (x * y) * math.exp(-(x2 + y2) / (2.0 * s2))
     
-    kern_yy_f = np.transpose(kern_xx_f, (1, 0))
+    # kern_yy_f = np.transpose(kern_xx_f, (1, 0))
     
     # convolution of each kernel
-    Dxx = sc.fftconvolve(src, kern_xx_f, mode='same') * scale * scale
-    Dxy = sc.fftconvolve(src, kern_xy_f, mode='same') * scale * scale
-    Dyy = sc.fftconvolve(src, kern_yy_f, mode='same') * scale * scale
+    # Dxx = sc.fftconvolve(src, kern_xx_f, mode='same') * scale * scale
+    # Dxy = sc.fftconvolve(src, kern_xy_f, mode='same') * scale * scale
+    # Dyy = sc.fftconvolve(src, kern_yy_f, mode='same') * scale * scale
+    Dxx = np.zeros(src.shape)
+    Dxy = np.zeros(src.shape)
+    Dyy = np.zeros(src.shape)
+    filters.gaussian_filter(src, (scale, scale), (0, 2), Dxx)
+    filters.gaussian_filter(src, (scale, scale), (1, 1), Dxy)
+    filters.gaussian_filter(src, (scale, scale), (2, 0), Dyy)
+    Dxx *= s2
+    Dxy *= s2
+    Dyy *= s2
     
-    return Dxx, Dxy, Dyy
-
-# Run 2D Hessian filter with parameter sigma on src, return the filtered image.
-def vesselness_2D(Dxx, Dxy, Dyy, beta, c):
     # eigenvalue calculations from Dxx, Dxy, Dyy and compute the vesselnes function
-    output = np.zeros((Dxx.shape))
+    output = np.zeros((src.shape))
     
     for x in range(Dxx.shape[0]):
         for y in range(Dxx.shape[1]):
-            
+            # ----------- using conventional way (faster) ------------- #
             dxx = Dxx[x, y]
             dxy = Dxy[x, y]
             dyy = Dyy[x, y]
             
-            # calculate eigenvalues
+            # # calculate eigenvalues
             tmp = dxx + dyy
             tmp2 = math.sqrt((dxx - dyy)**2 + 4*(dxy**2))
             
@@ -64,7 +69,18 @@ def vesselness_2D(Dxx, Dxy, Dyy, beta, c):
             
             lambda1 = mu1 if (abs(mu1) < abs(mu2)) else mu2
             lambda2 = mu2 if (abs(mu1) < abs(mu2)) else mu1
-
+            
+            # ----------- using numpy linalg eigendecomposition (slower) ------------- #
+            # hess_mat = np.zeros((2,2))
+            # hess_mat[0, 0] = dyy
+            # hess_mat[0, 1] = dxy
+            # hess_mat[1, 0] = dxy
+            # hess_mat[1, 1] = dxx
+            
+            # lam1, lam2 = np.linalg.eigvalsh(hess_mat)
+            # if (abs(lam1) > abs(lam2)):
+            #     lambda1, lambda2 = lam2, lam1
+           
             if(lambda2==0):
                 lambda2 = math.nextafter(0, 1)
             
@@ -74,18 +90,17 @@ def vesselness_2D(Dxx, Dxy, Dyy, beta, c):
             term2 = math.exp(-S2 / c)
             
             output[x, y] = term1 * (1.0 - term2) if (lambda2 < 0) else 0
-            
     return output
 
 def frangi_2D(src, B, C, start, stop, step):
+    # stores all the filtered images based on different scales
     all_filters = []
     
     beta = 2 * (B**2)
     c    = 2* (C**2)
     scale_range = np.arange(start, stop, step)
     for scale in scale_range:
-        Dxx, Dxy, Dyy = frangi_hessian(src, scale)
-        filtered_img = vesselness_2D(Dxx, Dxy, Dyy, beta, c)
+        filtered_img = vesselness_2D(src, scale, beta, c)
         all_filters.append(filtered_img)
     
     # pick the pixels with the highest vesselness value
@@ -103,61 +118,84 @@ def frangi_2D(src, B, C, start, stop, step):
     return output_img
         
 
-def vesselness_3D(Dxx, Dxy, Dyy, beta, c):
+def vesselness_3D(src, scale, alpha, beta, c):
     # eigenvalue calculations from Dxx, Dxy, Dyy and compute the vesselnes function
-    output = np.zeros((Dxx.shape))
+    s3 = scale * scale * scale
     
-    for x in range(Dxx.shape[0]):
-        for y in range(Dxx.shape[1]):
-            for z in range(Dxx.shape[2]):
+    D = np.zeros((src.shape[0], src.shape[1], src.shape[2], 3,3))
+    
+    filters.gaussian_filter(src, (scale, scale, scale), (0, 0, 2), D[:, :, :, 2,2])
+    filters.gaussian_filter(src, (scale, scale, scale), (0, 1, 1), D[:, :, :, 1,2])
+    filters.gaussian_filter(src, (scale, scale, scale), (0, 2, 0), D[:, :, :, 1,1])
+    filters.gaussian_filter(src, (scale, scale, scale), (2, 0, 0), D[:, :, :, 0,0])
+    filters.gaussian_filter(src, (scale, scale, scale), (1, 0, 1), D[:, :, :, 0,2])
+    filters.gaussian_filter(src, (scale, scale, scale), (1, 1, 0), D[:, :, :, 0,1])
+    D[:, :, :, 2,1] = D[:, :, :, 1,2]
+    D[:, :, :, 1,0] = D[:, :, :, 0,1]
+    D[:, :, :, 2,0] = D[:, :, :, 0,2]
+    
+    # normalization
+    D *= s3
+    
+    output = np.zeros((src.shape))
+    
+    for x in range(src.shape[0]):
+        for y in range(src.shape[1]):
+            for z in range(src.shape[2]):
             
-                dxx = Dxx[x, y, z]
-                dxy = Dxy[x, y, z]
-                dyy = Dyy[x, y, z]
+                lambda3, lambda2, lambda1 = lin.eigvalsh(D[z, y, x, :, :])
+                if (abs(lambda1) > abs(lambda2)):
+                    lambda2, lambda1 = lambda1, lambda2
+                if (abs(lambda2) > abs(lambda3)):
+                    lambda3, lambda2 = lambda2, lambda3
+                    
+                if (lambda3==0):
+                    lambda3 = math.nextafter(0,1)
+                if (lambda2 == 0):
+                    lambda2 = math.nextafter(0,1)
                 
-                tmp = dxx - dyy
-                tmp2 = math.sqrt(tmp * tmp + (4 * dxy * dxy))
+                Rb2 = np.float128((lambda1**2)/(lambda2 * lambda3))             # Rb2 tends to get very large -> use of float128
+                Ra = lambda2 / lambda3
+                S2 = (lambda1**2) + (lambda2**2) + (lambda3**2)
                 
-                mu1 = 0.5 * (dxx + dyy + tmp2)
-                mu2 = 0.5 * (dxx + dyy - tmp2)
+                term1 = math.exp(-(Ra**2) / alpha)
+                try: 
+                    term2 = np.exp(-Rb2 / beta)
+                except OverflowError:
+                    term2 = float('inf')
+                    print('yes')
+                term3 = math.exp(-S2 / c)
                 
-                lambda1 = mu1 if (abs(mu1) < abs(mu2)) else mu2
-                lambda2 = mu2 if (abs(mu1) < abs(mu2)) else mu1
-                if(lambda2==0):
-                    lambda2 = math.nextafter(0, 1)
-                
-                Rb = lambda1/lambda2
-                S2 = (lambda1**2) + (lambda2**2)
-                term1 = math.exp(-(Rb**2) / beta)
-                term2 = math.exp(-S2 / c)
-                
-                output[x, y] = term1 * (1.0 - term2) if (lambda2 < 0) else 0
+                output[x, y, z] = (1.0 - term1) * (term2) * (1.0 - term3) if (lambda2 <= 0 and lambda3 <= 0) else 0
             
     return output
 
-def frangi_3D(src, B, C, start, stop, step):
-
+def frangi_3D(src, A, B, C, start, stop, step):
     all_filters = []
     
-    beta = 2 * (B**2)
-    c    = 2* (C**2)
+    beta  = 2 * (B**2)
+    c     = 2 * (C**2)
+    alpha = 2 * (A**2)
     scale_range = np.arange(start, stop, step)
     for scale in scale_range:
-        Dxx, Dxy, Dyy = frangi_hessian(src, scale)
-        filtered_vol = vesselness_3D(Dxx, Dxy, Dyy, beta, c)
+        print('Scale: ' + str(scale) + ' started ...')
+        start = time.time()
+        filtered_vol = vesselness_3D(src, scale, alpha, beta, c)
         all_filters.append(filtered_vol)
+        print('Scale: ' + str(scale) + ' finished. \t Execution time: ' + str(time.time() - start))
     
     # pick the pixels with the highest vesselness value
-    max_img = all_filters[0]
+    max_vol = all_filters[0]
     output_vol = np.zeros(src.shape)
     
     for x in range(src.shape[0]):
         for y in range(src.shape[1]):
-            max_value = max_img[x, y]
-            for img in all_filters:
-                if (img[x, y] > max_value):
-                    max_value = img[x, y]
-            output_vol[x, y] = max_value
+            for z in range(src.shape[2]):
+                max_value = max_vol[x, y, z]
+                for vol in all_filters:
+                    if (vol[x, y, z] > max_value):
+                        max_value = vol[x, y, z]
+                output_vol[x, y] = max_value
     
     return output_vol
 

segmentation/prec_recall_curves.py
@@ -13,11 +13,13 @@ TPR       = np.zeros((2, 256))
 FPR       = np.zeros((2, 256))
 recall    = np.zeros((2, 256))
 precision = np.zeros((2, 256))
-
-for t in range(255):
+#%%
+for t in range(256):
+    # global thresholding for KESM data
     threshed_kesm = (kesm_vol <= t)
     met_kesm = mt.metric(kesm_grtruth, threshed_kesm)
     
+    # global thresholding for Micro-CT data
     threshed_micro = (micro_vol >= t)
     met_micro = mt.metric(micro_grtruth, threshed_micro)
     
@@ -31,55 +33,21 @@ for t in range(255):
     precision[1, t] = met_micro.precision()
     recall[1, t] = TPR[1, t]
     
+    if (t < np.min(kesm_vol)):
+        precision[0, t] = 1
+        
 #%%
 # plt.figure(1)
-plt.plot(recall[1], precision[1], marker='.', label='Micro-CT')
 plt.plot(recall[0], precision[0], marker='.', label='KESM')
+plt.plot(recall[1], precision[1], marker='.', label='Micro-CT')
 
 plt.xlabel('Recall')
 plt.ylabel('Precision')
 
 plt.legend(loc='lower left')
-
-# plt.figure(2)
-# plt.plot(FPR[0], TPR[0], label='KESM')
-# plt.plot(FPR[1], TPR[1], label='Micro-CT')
-
-# plt.xlabel('FPR')
-# plt.ylabel('TPR')
-
-# plt.legend(loc='lower left')
-plt.show
-#%%
-# plot the intensity histogram of Micro-ct volume data
-flat_volume = micro_vol.flatten()
-
-# Plot the intensity histogram
-n, bins, patches = plt.hist(flat_volume, bins=100)
-plt.xlabel('Intensity')
-plt.ylabel('Frequency')
-plt.title('Intensity Histogram')
-
-plt.axvline(x=bins[np.argmax(n)], color='r', linestyle='dashed', linewidth=2)
-plt.text(bins[np.argmax(n)], -190000, round(bins[np.argmax(n)],2), rotation=270, color='red', ha='center')
 plt.grid()
 
 plt.show()
 
-#%%
-# plot the intensity histogram of KESM volume data
-flat_volume = kesm_vol.flatten()
-
-# Plot the intensity histogram
-n, bins, patches = plt.hist(kesm_vol.ravel(), 256)
-plt.xlabel('Intensity')
-plt.ylabel('Frequency')
-plt.title('Intensity Histogram of KESM')
-
-plt.axvline(x=bins[np.argmax(n)], color='r', linestyle='dashed', linewidth=2)
-plt.text(bins[np.argmax(n)], -30000, round(bins[np.argmax(n)],2), rotation=270, color='red', ha='center')
-plt.grid()
-
-plt.show()
 
 

segmentation/test_frangi.py
@@ -11,10 +11,18 @@ import cv2
 import matplotlib.pyplot as plt
 import time
 #%%
+# test for 2D image
+brain = cv2.imread('brain.bmp', cv2.IMREAD_GRAYSCALE )
+#%%
+start = time.time()
+result = frg.frangi_2D(brain, 1, 20, 1, 3, 0.5)
+print(time.time() - start)
+#%%
+plt.imshow(result, cmap='gray')
+#%%
+# test for 3D volume
 micro_vol = np.load('micro_volume.npy')
 #%%
 start = time.time()
-result = frg.frangi_2D(micro_vol[0], 1, 20, 1, 3, 0.5)
+result = frg.frangi_3D(micro_vol, 1, 1, 20, 1, 2, 0.5)
 print(time.time() - start)
-
-plt.imshow(result, cmap='gray')