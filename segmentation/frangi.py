3567fd114b42339a19edd8d6650cfb85d83538a5
Helia Gohar <helia77@users.noreply.github.com>
2024-04-26 12:20:29 -0500

    Extra code removed

--- segmentation/frangi.py
+++ segmentation/frangi.py
@@ -143,207 +143,6 @@ def eigens(src, scale):
     #sprint('Eigen Done.')
     return lambdas
 
-def terms_alpha(src, scale, beta, c, back='white'):
-    lambdas = eigens(src, scale)
-    output = np.zeros(lambdas.shape)
-    for x in range(lambdas.shape[2]):
-        for y in range(lambdas.shape[1]):
-            for z in range(lambdas.shape[0]):
-                l1, l2, l3 = sorted(lambdas[z, y, x], key=abs)
-                if (back=='white' and (l2 < 0 or l3 < 0)):
-                    continue
-                elif(back=='black' and (l2 > 0 or l3 > 0)):
-                    continue
-                else:
-                    if (l3 == 0):
-                        l3 = math.nextafter(0,1)
-                    if (l2 == 0):
-                         l2 = math.nextafter(0,1)
-                    
-                    Ra2 = (l2 / l3)**2
-                    Rb2 = (l1**2)/(l2 * l3)
-                    S2 = (l1**2) + (l2**2) + (l3**2)
-                    
-                    term2 = np.exp(-Rb2 / beta)
-                    term3 = math.exp(-S2 / c)
-                    
-                    output[z, y, x, 0] = Ra2
-                    output[z, y, x, 1] = term2
-                    output[z, y, x, 2] = (1.0 - term3)
-    print('terms calculation done.')
-    return output
-    
-def terms_alpha_only(src, scale, back='white'):
-    lambdas = eigens(src, scale)
-    output = np.zeros(lambdas.shape)
-    for x in range(lambdas.shape[2]):
-        for y in range(lambdas.shape[1]):
-            for z in range(lambdas.shape[0]):
-                l1, l2, l3 = sorted(lambdas[z, y, x], key=abs)
-                if (back=='white' and (l2 < 0 or l3 < 0)):
-                    continue
-                elif(back=='black' and (l2 > 0 or l3 > 0)):
-                    continue
-                else:
-                    if (l3 == 0):
-                        l3 = math.nextafter(0,1)
-                    
-                    Ra2 = (l2 / l3)**2
-                    
-                    output[z, y, x, 0] = Ra2
-                    output[z, y, x, 1] = 1
-                    output[z, y, x, 2] = 1
-    print('terms calculation done.')
-    return output
-
-def terms_beta(src, scale, alpha, c, back='white'):
-    lambdas = eigens(src, scale)
-    output = np.zeros(lambdas.shape)
-    for x in range(lambdas.shape[2]):
-        for y in range(lambdas.shape[1]):
-            for z in range(lambdas.shape[0]):
-                l1, l2, l3 = sorted(lambdas[z, y, x], key=abs)
-                if (back=='white' and (l2 < 0 or l3 < 0)):
-                    continue
-                elif(back=='black' and (l2 > 0 or l3 > 0)):
-                    continue
-                else:
-                    if (l3 == 0):
-                        l3 = math.nextafter(0,1)
-                    if (l2 == 0):
-                        l2 = math.nextafter(0,1)
-                    
-                    Ra2 = (l2 / l3)**2
-                    Rb2 = (l1**2)/(l2 * l3)
-                    S2  = (l1**2) + (l2**2) + (l3**2)
-                    
-                    term1 = math.exp(-(Ra2) / alpha)
-                    term3 = math.exp(-S2 / c)
-                    
-                    output[z, y, x, 0] = (1.0 - term1)
-                    output[z, y, x, 1] = Rb2
-                    output[z, y, x, 2] = (1.0 - term3)
-    print('terms calculation done.')
-    return output
-
-def terms_beta_only(src, scale, back='white'):
-    lambdas = eigens(src, scale)
-    output = np.zeros(lambdas.shape)
-    for x in range(lambdas.shape[2]):
-        for y in range(lambdas.shape[1]):
-            for z in range(lambdas.shape[0]):
-                l1, l2, l3 = sorted(lambdas[z, y, x], key=abs)
-                if (back=='white' and (l2 < 0 or l3 < 0)):
-                    continue
-                elif(back=='black' and (l2 > 0 or l3 > 0)):
-                    continue
-                else:
-                    if (l3 == 0):
-                        l3 = math.nextafter(0,1)
-                    if (l2 == 0):
-                        l2 = math.nextafter(0,1)
-                    
-                    Rb2 = (l1**2)/(l2 * l3)
-                    
-                    output[z, y, x, 0] = 1
-                    output[z, y, x, 1] = Rb2
-                    output[z, y, x, 2] = 1
-    print('terms calculation done.')
-    return output
-
-def terms_c_only(src, scale, back='white'):
-    lambdas = eigens(src, scale)
-    output = np.zeros(lambdas.shape)
-    for x in range(lambdas.shape[2]):
-        for y in range(lambdas.shape[1]):
-            for z in range(lambdas.shape[0]):
-                l1, l2, l3 = sorted(lambdas[z, y, x], key=abs)
-                if (back=='white' and (l2 < 0 or l3 < 0)):
-                    continue
-                elif(back=='black' and (l2 > 0 or l3 > 0)):
-                    continue
-                else:
-                    S2  = (l1**2) + (l2**2) + (l3**2)
-                    
-                    output[z, y, x, 0] = 1
-                    output[z, y, x, 1] = 1
-                    output[z, y, x, 2] = S2
-    print('terms calculation done.')
-    return output
-
-def terms_c(src, scale, alpha, beta, back='white'):
-    lambdas = eigens(src, scale)
-    output = np.zeros(lambdas.shape)
-    for x in range(lambdas.shape[2]):
-        for y in range(lambdas.shape[1]):
-            for z in range(lambdas.shape[0]):
-                l1, l2, l3 = sorted(lambdas[z, y, x], key=abs)
-                if (back=='white' and (l2 < 0 or l3 < 0)):
-                    continue
-                elif(back=='black' and (l2 > 0 or l3 > 0)):
-                    continue
-                else:
-                    if (l3 == 0):
-                        l3 = math.nextafter(0,1)
-                    if (l2 == 0):
-                        l2 = math.nextafter(0,1)
-                    
-                    Ra2 = (l2 / l3)**2
-                    Rb2 = (l1**2)/(l2 * l3)
-                    S2  = (l1**2) + (l2**2) + (l3**2)
-                    
-                    term1 = math.exp(-(Ra2) / alpha)
-                    term2 = np.exp(-Rb2 / beta)
-                    
-                    output[z, y, x, 0] = (1.0 - term1)
-                    output[z, y, x, 1] = term2
-                    output[z, y, x, 2] = S2
-    print('terms calculation done.')
-    return output
-def vesselnese_alpha(values, alpha):
-    output = np.zeros((values.shape[0], values.shape[1], values.shape[2]))    
-    
-    for x in range(values.shape[2]):
-        for y in range(values.shape[1]):
-            for z in range(values.shape[0]):
-                Ra2   = values[z, y, x, 0]
-                term1 = math.exp(-Ra2 / alpha)
-                term2 = values[z, y, x, 1]
-                term3 = values[z, y, x, 2]
-                
-                output[z, y, x] = (1.0 - term1) * (term2) * (term3)
-            
-    return np.uint8(output * 255)
-
-def vesselnese_beta(values, beta):
-    output = np.zeros((values.shape[0], values.shape[1], values.shape[2]))    
-    
-    for x in range(values.shape[2]):
-        for y in range(values.shape[1]):
-            for z in range(values.shape[0]):
-                Rb2   = values[z, y, x, 1]
-                term1 = values[z, y, x, 0]
-                term2 = np.exp(np.float64(-Rb2 / beta))    # term2 tends to get very large -> use float64
-                term3 = values[z, y, x, 2]
-                
-                output[z, y, x] = (term1) * (term2) * (term3)
-            
-    return np.uint8(output * 255)
-
-def vesselnese_c(values, c):
-    output = np.zeros((values.shape[0], values.shape[1], values.shape[2]))    
-    
-    for x in range(values.shape[2]):
-        for y in range(values.shape[1]):
-            for z in range(values.shape[0]):
-                S2   = values[z, y, x, 2]
-                term1 = values[z, y, x, 0]
-                term2 = values[z, y, x, 1]
-                term3 = math.exp(-S2 / c)
-                
-                output[z, y, x] = (term1) * (term2) * (1.0 - term3)
-            
-    return np.uint8(output * 255)
 
 def highest_pixel(all_filters):
     max_vol = all_filters[0]
@@ -359,104 +158,30 @@ def highest_pixel(all_filters):
     
     return output_vol
 
-def process_alpha(A, terms, sample_gr):
-    alpha = 2 * (A**2)
-    vesselness_1 = vesselnese_alpha(terms[0], alpha)
-    vesselness_2 = vesselnese_alpha(terms[1], alpha)
-    vesselness_3 = vesselnese_alpha(terms[2], alpha)
-    vesselness_4 = vesselnese_alpha(terms[3], alpha)
-    #vesselness_5 = vesselnese_alpha(terms[4], alpha)
-    all_filters = [vesselness_1, vesselness_2, vesselness_3, vesselness_4]#, vesselness_5]
-
-    output = highest_pixel(all_filters)             # outputed volume for this alpha value
-
-    # apply otsu's threshold
-    thresh_volume, best_thresh = th.compute_otsu_img(output,  background='black')
-    print("\nThresh: ", best_thresh)
-    # calculate metrics
-    met_otsu = mt.metric(sample_gr, thresh_volume)
-    #print('.', end='')
-    return np.uint8(output), thresh_volume, met_otsu
-
-def process_beta(B, terms, sample_gr):
-    beta = 2 * (B**2)
-    vesselness_1 = vesselnese_beta(terms[0], beta)
-    vesselness_2 = vesselnese_beta(terms[1], beta)
-    vesselness_3 = vesselnese_beta(terms[2], beta)
-    vesselness_4 = vesselnese_beta(terms[3], beta)
-    #vesselness_5 = vesselnese_beta(terms[4], beta)
-    all_filters = [vesselness_1, vesselness_2, vesselness_3, vesselness_4]#, vesselness_5]
-
-    output = highest_pixel(all_filters)
-
-    # apply otsu's threshold
-    thresh_volume, best_thresh = th.compute_otsu_img(output,  background='black')
-    print("\nThresh: ", best_thresh)
-    # calculate metrics
-    met_otsu = mt.metric(sample_gr, thresh_volume)
-    #print('.', end='')
-    return np.uint8(output), thresh_volume, met_otsu
-
-def process_c(C, terms, sample_gr):
-    c = 2 * (C**2)
-    vesselness_1 = vesselnese_c(terms[0], c)
-    vesselness_2 = vesselnese_c(terms[1], c)
-    vesselness_3 = vesselnese_c(terms[2], c)
-    vesselness_4 = vesselnese_c(terms[3], c)
-    #vesselness_5 = vesselnese_c(terms[4], c)
-    all_filters = [vesselness_1, vesselness_2, vesselness_3, vesselness_4]#, vesselness_5]
-
-    output = highest_pixel(all_filters)
-
-    # apply otsu's threshold
-    # after Frangi's filter, the background is black
-    thresh_volume, best_thresh = th.compute_otsu_img(output,  background='black')
-    print("\nThresh: ", best_thresh)
-    # calculate metrics
-    met_otsu = mt.metric(sample_gr, thresh_volume)
-    #print('.', end='')
-    return output, thresh_volume, met_otsu
 
 def vesselness_3D(src, scale, alpha, beta, c, background):
-    s3 = scale * scale * scale
+    s2 = scale * scale
     
     # convolving image with Gaussian derivatives - including Dxx, Dxy, Dyy
     D = np.zeros((src.shape[0], src.shape[1], src.shape[2], 3,3))
     
-    start = time.time()
     filters.gaussian_filter(src, (scale, scale, scale), (0, 0, 2), D[:, :, :, 2,2])
-    #print('1st done: ', time.time() - start, ' seconds')
-    start = time.time()
     filters.gaussian_filter(src, (scale, scale, scale), (0, 1, 1), D[:, :, :, 1,2])
-    #print('2nd done: ', time.time() - start, ' seconds')
-    start = time.time()
     filters.gaussian_filter(src, (scale, scale, scale), (0, 2, 0), D[:, :, :, 1,1])
-    #print('3rd done: ', time.time() - start, ' seconds')
-    start = time.time()
     filters.gaussian_filter(src, (scale, scale, scale), (2, 0, 0), D[:, :, :, 0,0])
-    #print('4th done: ', time.time() - start, ' seconds')
-    start = time.time()
     filters.gaussian_filter(src, (scale, scale, scale), (1, 0, 1), D[:, :, :, 0,2])
-    #print('5th done: ', time.time() - start, ' seconds')
-    start = time.time()
     filters.gaussian_filter(src, (scale, scale, scale), (1, 1, 0), D[:, :, :, 0,1])
-    #print('6th done: ', time.time() - start, ' seconds')
     
     D[:, :, :, 2,1] = D[:, :, :, 1,2]
     D[:, :, :, 1,0] = D[:, :, :, 0,1]
     D[:, :, :, 2,0] = D[:, :, :, 0,2]
-    #print('Gaussian done.')
+
     # normalization
-    D *= s3
+    D *= s2
 
     output = np.zeros((src.shape))
-    #print('eigendecoposition: ...', end=' ')
-    start = time.time()
     lambdas = lin.eigvalsh(D)
-    #print(' Done.')
-    #print('Execution time: ', time.time() - start, ' seconds')
-    
-    
+
     for x in range(src.shape[2]):
         for y in range(src.shape[1]):
             for z in range(src.shape[0]):
@@ -510,7 +235,7 @@ def upgrade_vesselness(src, A, B, C, scale_range, background):
         filters.gaussian_filter(src, (scale, scale, scale), (1, 1, 0), Hzy)
         
         # correct for scaling - normalization
-        s3 = scale * scale * scale
+        s3 = scale * scale
         Hxx *= s3; Hyy *= s3; Hzz *= s3
         Hxy *= s3; Hxz *= s3; Hzy *= s3
         
@@ -583,16 +308,14 @@ def frangi_3D(src, A, B, C, scale_range, background='white'):
     c     = 2 * (C**2)
     alpha = 2 * (A**2)
     for scale in scale_range:
-        #print('\nScale: ' + str(scale) + ' started ...')
-        #start = time.time()
+
         filtered_vol = vesselness_3D(src, scale, alpha, beta, c, background)
         all_filters.append(filtered_vol)
-        #print('\nScale: ' + str(scale) + ' finished. \nExecution time: ' + str(time.time() - start))
-    
-    # pick the pixels with the highest vesselness value
+        
+    # select pixels with the highest vesselness value
     max_vol = all_filters[0]
     output_vol = np.zeros(src.shape)
-    #print('getting maximum pixels...')
+    
     for x in range(src.shape[2]):
         for y in range(src.shape[1]):
             for z in range(src.shape[0]):
@@ -626,7 +349,7 @@ def beyond_frangi_filter(src, tau, scale_range, background):
         filters.gaussian_filter(src, (s, s, s), (1, 1, 0), Hzy)
     
         # correct for scaling - normalization
-        s3 = s * s * s
+        s3 = s * s
         Hxx *= s3; Hyy *= s3; Hzz *= s3
         Hxy *= s3; Hxz *= s3; Hzy *= s3
         
