2a78b74330843285fe500da50f0232435f40e2b3
Helia Gohar <helia77@users.noreply.github.com>
2024-08-23 16:10:09 -0500

    Kline's optimization added

--- centerline extraction/costVote.py
+++ centerline extraction/costVote.py
@@ -12,7 +12,10 @@ import scipy as sp
 from scipy.optimize import fmin_powell
 import scipy.optimize as opt
 import subprocess
+import skeletonization as skelet
 
+
+#%%
 def pointcloud(self, spacing):
         ls = self.linesegments()
         pc = []
@@ -20,7 +23,7 @@ def pointcloud(self, spacing):
             pc = pc + l.pointcloud(spacing)
         return pc
 
-######## convert SDP file to OBJ file for visualization ##########
+######## convert SDP file to OBJ file ##########
 def sdp2obj(output_name):
     # read vertex file
     with open(output_name+'Vertex.sdp', 'r') as inputfile:
@@ -43,26 +46,59 @@ def sdp2obj(output_name):
             num2 = int(num2)
             outputfile.write('l '+ str(num1+1) + ' ' + str(num2+1) + '\n')
 
-def cost_vote(params):
+def kline_vote(gr_segment, GT_tree):
+    start_ids = [[23, 76, 199], [8, 199, 162], [54, 199, 17], [8, 0, 133], [183, 0, 196],
+    	[60, 94, 199]] #, [159, 32, 0], [53, 199, 64]]
+    result = np.zeros_like(gr_segment)
+    for i, start in enumerate(start_ids):
+        kline, _ = skelet.skelet_kline(gr_segment, start, dist_map_weight=6, min_branch_to_root=10)
+        result = np.logical_or(kline, result)
+        name = str(i)
+        print('saving', name, '...')
+        #md.npy2nrrd(kline, 'kline_filename_'+name+'.nrrd')
+        #md.npy2obj(kline, 'kline_'+name+'.obj')
+    
+    # md.npy2nrrd(result, 'kline_filename.nrrd')
+    # P_T = np.argwhere(kline).astype(np.float32)/199.0
+    
+    # T_tree = sp.spatial.cKDTree(P_T)
+    # # Query each KD tree to get the corresponding geometric distances
+    # [T_dist, _] = GT_tree.query(P_T)
+    # [GT_dist, _] = T_tree.query(GR)
+    # sigma = 0.01
+    # threshold = 0.5
+    # # convert distances to Gaussian metrics
+    # T_metric = np.exp(-0.5 * (T_dist ** 2 / sigma ** 2))
+    # T_metric[T_metric > threshold] = 1
+    # T_metric[T_metric <= threshold] = 0
+    # GT_metric = np.exp(-0.5 * (GT_dist ** 2 / sigma ** 2))
+    # GT_metric[GT_metric > threshold] = 1
+    # GT_metric[GT_metric <= threshold] = 0
+    # recall = np.mean(GT_metric)
+    # precision = np.mean(T_metric)
+    # fscore = (2*precision*recall)/(precision+recall)
+    # print(f'Recall={recall:.3f}, Precision={precision:.3f}, \tFscore={fscore:.3f}')
+    return result
+
+#%%
+def cost_vote(dilate, deltag, radius, th, GT_tree):
     # dilate distance of the confidence voxels, threshold in the confidence estimation (included), 
     # the param to consider interval of distances, radius used to compute the accumulation
-    dilate, deltaG, radius = params
-    print('Dilate={},\tDeltaG={},\tradius={}'.format(round(dilate,4), round(deltaG,4), round(radius,4)))
-    start = time.time()
+    print('Dilate={},\tDeltaG={},\tRadius={},\tThresh={}'.format(round(dilate,4), round(deltag,4), round(radius, 4), round(th, 2)))
     # run the C++ code on the sample_gr binary .OFF file and obtain the centerline as SDP file
     exe_file = 'CDCVAM/build/bin/Release/centerLineGeodesicGraph.exe'
-    output_name = "sample_gr"
+    output_name = "lsm_gr_test"
     if os.path.isfile(output_name+'Vertex.sdp'):
         os.remove(output_name+'Vertex.sdp')
         os.remove(output_name+'Edges.sdp')
     command = [
         exe_file,
-        '-i', 'small_gr_fixed_for_test.off',
+        '-i', 'Data/LSM/lsm_brain_gr_100x100.off',
         "-o", output_name,
         "--dilateDist", str(dilate),
-        "-g", str(deltaG),
+        "-g", str(deltag),
         "-R", str(radius),
-        "-t", "0.5"
+        "-t", str(th)
     ]
     
     # Execute the command
@@ -70,69 +106,146 @@ def cost_vote(params):
     
     # now we have SDP files saved in the directory
     sdp2obj(output_name)
-    gr_skeleton = md.nrrd_to_numpy('centerline.nrrd')
     T = md.NWT(output_name+'_centerline.obj')
-    GR = np.argwhere(gr_skeleton).astype(np.float32)
-    
-    sigma = 0.01
-    subdiv = 2
+      
     # generate point clouds representing both networks
-    P_T = np.array(T.pointcloud(sigma/subdiv))
-    P_T /= 99.0
-    GR[:, 0] /= 1023.0
-    GR[:, 1:3] /= 511.0
-    
+    P_T = np.array(T.pointcloud(0.1))/199.0
+    print('Point clouds')
     # generate KD trees for each network
     T_tree = sp.spatial.cKDTree(P_T)
-    GT_tree = sp.spatial.cKDTree(GR)
-    
+    print('T tree')
     # query each KD tree to get the corresponding geometric distances
     [T_dist, _] = GT_tree.query(P_T)
     [GT_dist, _] = T_tree.query(GR)
+    print('Query')
     
     # convert distances to Gaussian metrics
     threshold = 0.5
+    sigma = 0.01
     T_metric = np.exp(-0.5 * (T_dist ** 2 / sigma ** 2))
     T_metric[T_metric > threshold] = 1
     T_metric[T_metric <= threshold] = 0
-    
+    print('T Metrics')
     GT_metric = np.exp(-0.5 * (GT_dist ** 2 / sigma ** 2))
     GT_metric[GT_metric > threshold] = 1
     GT_metric[GT_metric <= threshold] = 0
-    
+    print('G Metrics')
     # we try to minimize the False Negative and Positive Rates combined based on the input parameters
-    FNR = 1 - np.mean(GT_metric)
-    FPR = 1 - np.mean(T_metric)
-    print('FNR={}, FPR={}, \tTOTAL={}\t time:{} mins'.format(FNR, FPR, FNR+FPR, (time.time()-start)/60.0))
-    return FNR+FPR
-    
-    
-initial_params = (2, 3, 5)
-start = time.time()
-#result_b = fmin_powell(cost_vote, initial_params)
-result = opt.minimize(cost_vote, initial_params, method="Powell", bounds=((0.5, 10), (0.5, 10), (0.5, 5)))
-print('Took {} minutes.'.format((time.time() - start)/60.0))
+    recall = np.mean(GT_metric)
+    precision = np.mean(T_metric)
+    fscore = (2*precision*recall)/(precision+recall)
+    print(f'Recall={recall:.3f}, Precision={precision:.3f}, \tFscore={fscore:.3f}\n')
+    return fscore
 
 #%%
-print('dialte:\t', result[0])
-print('deltaG:\t', result[1])
-print('radius:\t', result[2])
-print('thresh:\t', result[2])
-#%%
-output_name = "sample_gr"
-dilate, radius = [2, 10]
-exe_file = 'CDCVAM/build/bin/Release/centerLineGeodesicGraph.exe'
-command = [
-    exe_file,
-    '-i', 'sample_gr.off',
-    "-o", output_name,
-    "--dilateDist", '2',
-    "-g", '3',
-    "-R", '10',
-    "-t", '0.5'
-]
+# for KESM data
+# gr_skeleton = md.nrrd2npy('centerline.nrrd')
+# GR = np.argwhere(gr_skeleton).astype(np.float32)
+# GR[:, 0] /= 1023.0
+# GR[:, 1:3] /= 511.0
+# GT_tree = sp.spatial.cKDTree(GR)
+# gr_segment = np.load('../segmentation/sample_gr.npy')
 
-# Execute the command
-start = time.time()
-subprocess.run(command, check=True)
-print('Took {} minutes.'.format((time.time() - start)/60.0))
+# for Micro data
+# GR = np.load('Data/Micro/micro_centerlines_GR_float32.npy')
+# GR /= 199.0
+# GT_tree = sp.spatial.cKDTree(GR)
+# gr_segment = np.load('../segmentation/Data/Micro-CT/sample_gr_micro.npy')
+
+# for LSM data
+gr_skeleton = md.nrrd2npy('Data/LSM/new/lsm_brain_centerline.nrrd')
+GR = np.argwhere(gr_skeleton).astype(np.float32)
+GR /= 199.0
+GT_tree = sp.spatial.cKDTree(GR)
+gr_segment = np.load('Data/LSM/lsm_brain_gr.npy')
+
+#%%
+# perform optimizing Kerautret's method
+dilate = np.linspace(1, 1.5, 3)
+delta = np.linspace(2.75, 3.5, 3)
+radius = np.linspace(0.7, 1.5, 3)
+thresh = [0.5]
+best = 0
+#%%
+for dil in dilate:
+    for dl in delta:
+        for rad in radius:
+            for th in thresh:
+                fscore = cost_vote(dil, dl, rad, th, GT_tree)
+                if fscore > best:
+                    best = fscore
+                    best_param = [dil, dl, rad, th]
+                    
+#%%
+#ehem = cost_vote(1.1, 2.75, 0.8, 0.6, GT_tree)
+#%%
+result = kline_vote(gr_segment, GT_tree)
+
+#%%
+md.npy2obj(np.uint8(result), 'Data/Kline/kline_ground_lsm.obj')
+P_T = np.argwhere(result).astype(np.float32)/199.0
+
+T_tree = sp.spatial.cKDTree(P_T)
+# Query each KD tree to get the corresponding geometric distances
+[T_dist, _] = GT_tree.query(P_T)
+[GT_dist, _] = T_tree.query(GR)
+sigma = 0.01
+threshold = 0.5
+# convert distances to Gaussian metrics
+T_metric = np.exp(-0.5 * (T_dist ** 2 / sigma ** 2))
+T_metric[T_metric > threshold] = 1
+T_metric[T_metric <= threshold] = 0
+GT_metric = np.exp(-0.5 * (GT_dist ** 2 / sigma ** 2))
+GT_metric[GT_metric > threshold] = 1
+GT_metric[GT_metric <= threshold] = 0
+recall = np.mean(GT_metric)
+precision = np.mean(T_metric)
+fscore = (2*precision*recall)/(precision+recall)
+print(f'Recall={recall:.3f}, Precision={precision:.3f}, \tFscore={fscore:.3f}')
+
+#%%
+# check if all start IDs are 1 in all segmentations
+start_ids = [[23, 76, 199], [8, 199, 162], [54, 199, 17], [8, 0, 133], [183, 0, 196], [60, 94, 199]]
+list_name = ['binary_bfrangi_otsu3d.npy', 'binary_frangi_otsu3d.npy', 'binary_oof.npy', 'binary_otsu3d.npy', 'binary_otsu2d.npy', 'binary_unet.npy']
+
+for name in list_name:
+    vol = np.load('../segmentation/Data/Segmentations/LSM/' + name)
+    print_name = (name.split('_')[1]).split('.')[0]
+    # print(print_name)
+    for start in start_ids:
+        i, j, k = start
+        if vol[i, j, k] == 0:
+            print('Zero at', start, 'in ', print_name)
+    
+
+#%%
+results = np.zeros((200, 200, 200), dtype=np.uint8)
+files = [file for file in os.listdir('Data/Kline/npy files/')]
+for file in files:
+    centerline = np.load('Data/Kline/npy files/' + file)
+    results = np.logical_or(centerline, results)
+#np.save('Data/Kline/final_kline.npy', np.asarray(results))
+#%%
+results = np.load('Data/Kline/test/test_6_15.npy')
+P_T = np.argwhere(results).astype(np.float32)/199.0
+
+T_tree = sp.spatial.cKDTree(P_T)
+# Query each KD tree to get the corresponding geometric distances
+[T_dist, _] = GT_tree.query(P_T)
+[GT_dist, _] = T_tree.query(GR)
+sigma = 0.01
+threshold = 0.5
+# convert distances to Gaussian metrics
+T_metric = np.exp(-0.5 * (T_dist ** 2 / sigma ** 2))
+T_metric[T_metric > threshold] = 1
+T_metric[T_metric <= threshold] = 0
+GT_metric = np.exp(-0.5 * (GT_dist ** 2 / sigma ** 2))
+GT_metric[GT_metric > threshold] = 1
+GT_metric[GT_metric <= threshold] = 0
+recall = np.mean(GT_metric)
+precision = np.mean(T_metric)
+fscore = (2*precision*recall)/(precision+recall)
+print(f'Recall={recall:.4f}, Precision={precision:.4f}, \tFscore={fscore:.4f}')
+
+
+
